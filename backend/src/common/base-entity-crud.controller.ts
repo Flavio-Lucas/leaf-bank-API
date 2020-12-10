b675a8772c732d0a6eadf929d92c3df6ca693558
Vinícius Lourenço <vinyygamerlol@gmail.com>
2020-12-10 16:54:15 -0300

    fix(base-entity-crud): disabled enabled and disabled of admins

--- backend/src/common/base-entity-crud.controller.ts
+++ backend/src/common/base-entity-crud.controller.ts
@@ -1,13 +1,16 @@
 //#region Imports
 
-import { NotFoundException, Param, Put } from '@nestjs/common';
+import { BadRequestException, NotFoundException, Param, Put } from '@nestjs/common';
 import { ApiNotFoundResponse, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { CrudRequest, ParsedRequest } from '@nestjsx/crud';
 
 import { ProtectTo } from '../decorators/protect/protect.decorator';
-import { BaseEntity } from './base-entity';
+import { RolesEnum } from '../modules/auth/models/roles.enum';
+import { UserEntity } from '../modules/users/entities/user.entity';
+import { isAdminUser } from '../utils/functions';
 import { BaseCrudController } from './base-crud.controller';
 import { BaseCrudService } from './base-crud.service';
+import { BaseEntity } from './base-entity';
 
 //#endregion
 
@@ -35,18 +38,20 @@ export class BaseEntityCrudController<TEntity extends BaseEntity, TService exten
    * Método que desativa uma entidade
    *
    * @param id A identificação da entidade
-   * @param crudRequest As informações da requisição do CRUD
    */
-  @ProtectTo('admin')
+  @ProtectTo(RolesEnum.ADMIN)
   @Put('/:id/disable')
   @ApiOperation({ summary: 'Disable one Entity' })
   @ApiOkResponse({ description: 'The entity was disabled with successful.' })
   @ApiNotFoundResponse({ description: 'The entity was not found' })
-  public async disable(@Param('id') id: number, @ParsedRequest() crudRequest: CrudRequest): Promise<TEntity> {
+  public async disable(@Param('id') id: string): Promise<TEntity> {
     const entity = await this.service.repository.findOne(id);
 
     if (!entity)
       throw new NotFoundException('A entidade procurada não existe.');
+
+    if (entity instanceof UserEntity && isAdminUser(entity))
+      throw new BadRequestException('Você não tem permissão para desativar um usuário que também é administrador.');
 
     entity.isActive = false;
 
@@ -59,16 +64,19 @@ export class BaseEntityCrudController<TEntity extends BaseEntity, TService exten
    * @param id A identificação da entidade
    * @param crudRequest As informações da requisição do CRUD
    */
-  @ProtectTo('admin')
+  @ProtectTo(RolesEnum.ADMIN)
   @Put('/:id/enable')
   @ApiOperation({ summary: 'Enable one Entity' })
-  @ApiOkResponse({ description: 'The entity was disabled with successfull.' })
+  @ApiOkResponse({ description: 'The entity was disabled with successful.' })
   @ApiNotFoundResponse({ description: 'The entity was not found' })
-  public async enable(@Param('id') id: number, @ParsedRequest() crudRequest: CrudRequest): Promise<TEntity> {
+  public async enable(@Param('id') id: string, @ParsedRequest() crudRequest: CrudRequest): Promise<TEntity> {
     const entity = await this.service.repository.findOne(id);
 
     if (!entity)
       throw new NotFoundException('A entidade procurada não existe.');
+
+    if (entity instanceof UserEntity && isAdminUser(entity))
+      throw new BadRequestException('Você não tem permissão para ativar um usuário que também é administrador.');
 
     entity.isActive = true;
 
